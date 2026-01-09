e1c37d23e8868fb0835eb9fbb7b1eff182ac60ee
DustinHaaland544 <nasifshah544@gmail.com>
2026-01-09 14:16:34 -0400

    Tailwind Fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Meme Generator</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind Is Working 🚀
+      </h1>
     </div>
   );
 }
